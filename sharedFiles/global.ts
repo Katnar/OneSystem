a9480ad24b018993293dd04b51ea37cc0257e6fc
NadavHR <nadavh2611@gmail.com>
2026-09-24 10:08:15 +0300

    added the Year type

--- sharedFiles/global.ts
+++ sharedFiles/global.ts
@@ -1,8 +1,10 @@
 declare const brandSymbol: unique symbol;
-export type AssertOnly<T, UniqueTypeId extends string> = T & {
+export type AssertOnly<T, UniqueTypeId> = T & {
 	readonly [brandSymbol]: UniqueTypeId;
 };
 
 export type BrandedString<UniqueTypeId extends string> = AssertOnly<string, UniqueTypeId>;
 
 export type BrandedNumber<UniqueTypeId extends string> = AssertOnly<number, UniqueTypeId>;
+
+export type Year = BrandedNumber<"year"> 
