01b57c74bf13a7e9df38dda63531bfd10a4761ee
NadavHR <nadavh2611@gmail.com>
2026-09-24 10:28:20 +0300

    added the Meshek type

--- sharedFiles/meshek.ts
+++ sharedFiles/meshek.ts
@@ -1,6 +1,13 @@
-import { Mador } from "./global"
-
-
+import { BrandedString, Mador } from "./global"
 
 export const {Budget, ...MeshekMador} = Mador; 
 export type MeshekMador = typeof MeshekMador[keyof typeof MeshekMador]
+export type MeshekID = BrandedString<"meshek id">
+
+export type MeshekBaseFields = {
+    meshekName: string,
+    mador: MeshekMador
+}
+
+export type Meshek = { _id: MeshekID } & MeshekBaseFields
+
